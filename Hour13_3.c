6d20c12a1b7ad4376069fecdcabeb81870922936
Rimantas <rimantas.smitas@gmail.com>
2018-01-26 20:31:36 +0000

    Day 5

---
+++ Hour13_3.c
@@ -0,0 +1,22 @@
+/*3. Rewrite the program in Listing 13.4. This time, convert all uppercase characters to
+their lowercase counterparts.
+/* 13L04.c: Using gets() and puts() */
+#include <stdio.h>
+
+main()
+{
+char str[80];
+int i, delt = 'A' - 'a';
+
+printf("Enter a string less than 80 characters:\n");
+gets( str );
+i = 0;
+while (str[i]){
+if ((str[i] >= 'A') && (str[i] <= 'Z'))
+str[i] -= delt; /* convert to upper case */
+++i;
+}
+printf("The entered string is (in uppercase):\n");
+puts( str );
+return 0;
+}
